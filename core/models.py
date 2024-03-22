160ea2b8c4c220adf9a6684efb1985e1dcb3fb58
Cate <catefeho@gmail.com>
2024-03-22 09:54:07 -0300

    .

--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     nome= models.CharField(max_length=20)
-    @property
     def __str__(self) -> str:
         return self.nome
 
