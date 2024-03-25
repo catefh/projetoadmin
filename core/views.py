2dfabd416a1f8be84e7ed004732e9da967cca590
Cate <catefeho@gmail.com>
2024-03-25 10:11:22 -0300

    .

--- core/views.py
+++ core/views.py
@@ -13,9 +13,15 @@ def index(request):
     return render(request, 'index.html', context)
 
 def pesquisa(request, alim):
-
     busca = request.GET.get('alim')
     List = Produto.objects.filter(nome__contains=alim)
     if busca:
         List = Produto.objects.filter(nome__contains = busca)
+    return render(request, 'exibir.html', {'List': List})
+
+def nova(request, desc):
+    busca = request.GET.get('desc')
+    List = Produto.objects.filter(descricao__contains=desc)
+    if busca:
+        List = Produto.objects.filter(descricao__contains=busca)
     return render(request, 'exibir.html', {'List': List})
--- myadmin/urls.py
+++ myadmin/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('teste', views.index),
     path("<str:alim>/", views.pesquisa),
+    path("produtos/busca/str:alim>", views.nova),
+    path("produtos/busca/<str:desc>", views.nova),
 
 ]
